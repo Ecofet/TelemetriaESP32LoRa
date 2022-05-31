7563cfbcbe2b677e6a2aae6c10f4c5e10be4f00d
vdgaete <vdgaete@hotmail.com>
2022-05-31 10:02:45 -0300

    transmissão em json

--- PlatformIO/TransmissaoESPLoRa/src/main.cpp
+++ PlatformIO/TransmissaoESPLoRa/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <SPI.h>
 #include "heltec.h"
 extern "C" {
@@ -11,31 +12,32 @@ extern "C" {
 //#define TXD2 17
 
 #define BAND  915E6
-
+int value = 0;
 int counter = 0;
-
+DynamicJsonDocument doc(128);
+char buff[128];
+String bufff;
 
 void setup() {
-  // Observe que o formato para definir uma porta serial é o seguinte: Serial2.begin(taxa de transmissão, protocolo, pino RX, pino TX);
-  
   Serial.begin(115200);
-
   Serial.println("LoRa Sender");
-
   Heltec.begin(true, true, true, true, BAND);
 }
 
 void loop() { 
   Serial.print("Sending packet: ");
   Serial.println(counter);
+  doc["value"] =  rand()%100;
+  doc["counter"] =  counter;
+  serializeJson(doc,buff);
   LoRa.beginPacket();
   LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
-  LoRa.print("hello ");
-  LoRa.print(counter);
+  LoRa.print(buff);
   LoRa.endPacket();
   counter++;
   Serial.println("Packet sent");
-    digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
+  
+  digitalWrite(25, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(25, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
